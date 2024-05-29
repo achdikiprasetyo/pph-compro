3d4b4334365f9ec912593a221dc692de1b66f1fc
achdikiprasetyo <dikiprasetyo1234@gmail.com>
2024-05-29 11:30:36 +0700

    update jasa sumber daya manusia

--- resources/views/jasa/sdm.blade.php
+++ resources/views/jasa/sdm.blade.php
@@ -9,9 +9,9 @@
         <div class="left-container">
             <h1 class="title">Jasa Sumber Daya Manusia</h1>
             <p class="description">
-                Di dalam pelatihan Sumber Daya Manusia, kami mengundang anda untuk menjadi mitra dalam mengoptimalkan Sumber Daya Manusia yang ada di perusahaan anda. Perusahaan kami tidak hanya menawarkan jasa pelatihan yang standar, melainkan  juga melayani pelatihan yang disesuaikan dengan kebutuhan spesifik lainnya.
+                Di dalam pelatihan Sumber Daya Manusia, kami bertujuan untuk meningkatkan keterampilan, pengetahuan, dan kemampuan setiap individu dalam berbagai aspek yang berkaitan dengan pekerjaan mereka. Perusahaan kami tidak hanya menawarkan jasa pelatihan yang standar, melainkan  juga melayani pelatihan yang disesuaikan dengan kebutuhan spesifik lainnya.
             <p class="description">
-                Layanan kami dirancang untuk memberikan jasa pelatihan yang telah disesuaikan untuk kebutuhan perusahaan anda. Dengan pendekatan yang praktis dan mudah dimengerti, kami akan membantu perusahaan anda untuk meningkatkan keterampilan, motivasi dan produktivitas tim.
+                Layanan kami dirancang untuk memberikan jasa pelatihan yang telah disesuaikan untuk kebutuhan perusahaan anda. Dengan pendekatan yang praktis dan mudah dimengerti, kami akan membantu perusahaan anda untuk meningkatkan keterampilan, motivasi dan produktivitas individu.
         </div>
         <div class="right-container">
             <img src="{{asset('image/jasa/jasaSdm.png')}}" alt="Gambar">
